3967416df8a2ebc8548505b89b05a86566dfd72d
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-21 23:37:16 +0500

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
       style={{
         padding: "20px 40px",
         backgroundImage: `url(${backgroundImage})`,
-        position: "fixed",
-        bottom: 0,
+        // position: "fixed",
+        // bottom: 0,
         width: "100%",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -17,12 +17,12 @@ const Footer = () => {
         alignItems:"center"
       }}
     >
-      <p>
+      <p className="m-0">
         &#169; Copyright. All Rights Reserved. Mustan Painting and Decorating
       </p>
-      <p>
+      <p className="m-0">
         Website by{" "}
-        <span style={{ fontWeight: "bold", fontSize: "20px" }}>
+        <span  style={{ fontWeight: "bold", fontSize: "20px" }}>
           Ameer Hamza
         </span>
       </p>
