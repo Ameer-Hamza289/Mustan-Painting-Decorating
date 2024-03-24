05bb316032f0be0af878d487a6f6a86116f8a12e
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-24 19:55:41 +0500

    Home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import ContactComponent from "../components/ContactComponent";
+import Footer from "../components/Footer";
+import HomeNavbar from "../components/HomeNavbar";
 
 const Home = () => {
   return (
-    <div>
-      Hello from Home screen
-    </div>
-  )
-}
+      <div className="d-flex" style={{ height:"100vh"}}>
+        <HomeNavbar />
+        <div style={{ width:"94%", overflowY:"scroll"}}>
+            <div className="Home text-white p-5">
+            <div>
+              <p>0420 600 185</p>
+              <p>WELCOME TO</p>
+              <p>PERTH PAINTING</p>
+              <p>& DECO</p>
+            </div>
+          </div>
+          <ContactComponent />
+          <Footer />
+        </div>
+      </div>
+  );
+};
 
-export default Home
+export default Home;
