5a6f62eb06b81b9ace6d26e001bc9672faefc606
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-25 23:55:27 +0500

    Home page in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,29 @@ import HomeNavbar from "../components/HomeNavbar";
 
 const Home = () => {
   return (
-      <div className="d-flex" style={{ height:"100vh"}}>
-        <HomeNavbar />
-        <div style={{ width:"94%", overflowY:"scroll"}}>
-            <div className="Home text-white p-5">
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <HomeNavbar />
+      <div style={{ width: "94%", overflowY: "scroll" }}>
+        <div className="Home text-white p-5">
+          <div>
+            <span style={{ fontWeight: 700, fontSize: "24px" }}>
+              0420 600 185
+            </span>
+            <p className="fs-max">WELCOME TO</p>
+            <p className="fs-max">PERTH PAINTING</p>
+            <p className="fs-max">& DECO</p>
             <div>
-              <p>0420 600 185</p>
-              <p>WELCOME TO</p>
-              <p>PERTH PAINTING</p>
-              <p>& DECO</p>
+              <span>
+                +12 Years experience. Workmanship guaranteed & fixed price
+                quotes.
+              </span>
             </div>
           </div>
-          <ContactComponent />
-          <Footer />
         </div>
+        <ContactComponent />
+        <Footer />
       </div>
+    </div>
   );
 };
 
