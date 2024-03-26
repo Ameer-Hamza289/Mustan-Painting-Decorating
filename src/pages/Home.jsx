2f0e677d1c451f7dd5dfc09a31b48d3d323ec322
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-26 23:01:16 +0500

    Home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,27 @@
 import ContactComponent from "../components/ContactComponent";
 import Footer from "../components/Footer";
 import HomeNavbar from "../components/HomeNavbar";
+import HomeGallery from "../components/HomeGallery";
+import ExpectWIthUs from "../components/ExpectWIthUs";
+import Reviews from "../components/Reviews";
+import IdeaBrief from "../components/IdeaBrief";
+import Services from "../components/Services";
+import Performance from "../components/Performance";
+import WelcomeSection from "../components/WelcomeSection";
 
 const Home = () => {
   return (
     <div className="d-flex" style={{ height: "100vh" }}>
       <HomeNavbar />
       <div style={{ width: "94%", overflowY: "scroll" }}>
-        <div className="Home text-white p-5">
-          <div>
-            <span style={{ fontWeight: 700, fontSize: "24px" }}>
-              0420 600 185
-            </span>
-            <p className="fs-max">WELCOME TO</p>
-            <p className="fs-max">PERTH PAINTING</p>
-            <p className="fs-max">& DECO</p>
-            <div>
-              <span>
-                +12 Years experience. Workmanship guaranteed & fixed price
-                quotes.
-              </span>
-            </div>
-          </div>
+        <div className="Home text-white ">
+          <WelcomeSection />
+          <Services />
+          <Performance />
+          <HomeGallery />
+          <ExpectWIthUs />
+          <Reviews />
+          <IdeaBrief />
         </div>
         <ContactComponent />
         <Footer />
