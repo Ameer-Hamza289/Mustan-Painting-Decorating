227981210ae1e547b5ccdc171aa5dd7d03a4b67c
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-26 22:39:03 +0500

    Reviews UI added

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,48 @@
+import starRating from "../assets/rating-stars.png";
+
+const Reviews = () => {
+  return (
+    <>
+      <div className="py-5 " style={{ paddingInline: "70px" }}>
+        <p className="fw-bold mb-0 lh-1" style={{ fontSize: "46px" }}>
+          WHAT PEOPLE ARE SAYING
+        </p>
+        <p className="fw-bold" style={{ fontSize: "46px" }}>
+          ABOUT US...
+        </p>
+      </div>
+      <div className="Service1Home" style={{ paddingBlock: "80px", paddingInline:"20px" }}>
+        <div className="row">
+          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+            <img src={starRating} alt="star rating" height={25} />
+            <p className="mt-3">
+              David is a very professional, excellent Tradesman. Highly
+              recommend and would definitely use him again.
+            </p>
+            <p className="fw-bold mt-1">SCOTT</p>
+          </div>
+          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+            <img src={starRating} alt="star rating" height={25} />
+            <p className="mt-3">
+              Perth Painting & Deco just finished an excellent job painting our
+              new house. David is a top professional doing an excellent job,
+              with a very good price I definitely recommend him.
+            </p>
+            <p className="fw-bold mt-1">JC</p>
+          </div>
+          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+            <img src={starRating} alt="star rating" height={25} />
+            <p className="mt-3">
+              We have use Perth Painting & Deco 3 times now and cant fault them.
+              David has done a fantastic job every time, very tidy and a great
+              price. Would definitely recommend! Thanks David
+            </p>
+            <p className="fw-bold mt-1">ASHELY</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reviews;
