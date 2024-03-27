ce49a79f7c9a27dd9a2180bf6fb94489c2e11a3d
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-28 02:33:24 +0500

    Review responsiveness

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,17 +3,17 @@ import starRating from "../assets/rating-stars.png";
 const Reviews = () => {
   return (
     <>
-      <div className="py-5 " style={{ paddingInline: "70px" }}>
-        <p className="fw-bold mb-0 lh-1" style={{ fontSize: "46px" }}>
+      <div className="py-5  reviews" style={{ paddingInline: "70px" }}>
+        <p className="fw-bold mb-0 reviewHeading" style={{ fontSize: "46px", lineHeight:1.3 }}>
           WHAT PEOPLE ARE SAYING
         </p>
-        <p className="fw-bold" style={{ fontSize: "46px" }}>
+        <p className="fw-bold reviewHeading" style={{ fontSize: "46px" }}>
           ABOUT US...
         </p>
       </div>
       <div className="Service1Home" style={{ paddingBlock: "80px", paddingInline:"20px" }}>
         <div className="row">
-          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+          <div className="col-md-4 col-12 reviewCard " style={{ padding:"0px 50px" }}>
             <img src={starRating} alt="star rating" height={25} />
             <p className="mt-3">
               David is a very professional, excellent Tradesman. Highly
@@ -21,7 +21,7 @@ const Reviews = () => {
             </p>
             <p className="fw-bold mt-1">SCOTT</p>
           </div>
-          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+          <div className="col-md-4 col-12 reviewCard " style={{ padding:"0px 50px" }}>
             <img src={starRating} alt="star rating" height={25} />
             <p className="mt-3">
               Perth Painting & Deco just finished an excellent job painting our
@@ -30,7 +30,7 @@ const Reviews = () => {
             </p>
             <p className="fw-bold mt-1">JC</p>
           </div>
-          <div className="col-md-4 col-12 " style={{ padding:"0px 50px" }}>
+          <div className="col-md-4 col-12 reviewCard mb-0" style={{ padding:"0px 50px" }}>
             <img src={starRating} alt="star rating" height={25} />
             <p className="mt-3">
               We have use Perth Painting & Deco 3 times now and cant fault them.
