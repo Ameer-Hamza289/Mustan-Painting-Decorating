8df7b9de5a62bb1b4618fb4ff51c86344d2ade72
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-03-27 14:42:14 +0500

    Remove Unnecessary packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { persistor, store } from "./store/store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
@@ -10,24 +6,17 @@ import Services from "./pages/Services";
 import Gallery from "./pages/Gallery";
 
 const App = () => {
-
   return (
-    <Provider store={store}>
-      <ToastContainer />
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/contact" element={<ContactUs/>} />
-            <Route path="/about" element={<AboutUs/>} />
-            <Route path="/services" element={<Services/>} />
-            <Route path="/gallery" element={<Gallery/>} />
-          </Routes>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  )
-
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
